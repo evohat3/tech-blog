bed2624d5866e63f8688881b986d1f2a91798ca7
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-05-03 07:07:45 -0500

    initial structuring

---
+++ controllers/routes/Home.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+  res.render('home', {layout: 'index'});
+});
+
+
+module.exports = router
