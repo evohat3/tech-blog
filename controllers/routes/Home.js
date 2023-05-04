f00eee5e3e64e09802cf4e21c7893c698840f661
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-05-03 19:12:46 -0500

    update routs, views and public

--- controllers/routes/Home.js
+++ controllers/routes/Home.js
@@ -5,5 +5,9 @@ router.get('/', async (req, res) => {
   res.render('home', {layout: 'index'});
 });
 
+router.get('/signup', async (req,res) => {
+  res.render('signup', {layout: 'index'});
+});
+
 
 module.exports = router
---
+++ controllers/routes/User.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/dashboard', async (req, res) => {
+  
+  res.render('dashboard', {layout: 'index'});
+});
+
+router.get('/signup', async (req,res) => {
+  res.render('signup', {layout: 'index'});
+});
+
+
+module.exports = router
