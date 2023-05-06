51c2afe399162ec83f199c83edc8bb47edef813c
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-05-05 21:38:26 -0500

    created new routes

--- controllers/routes/Home.js
+++ controllers/routes/Home.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Blog, User } = require('../../models');
-// const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => { 
 
@@ -19,6 +19,7 @@ router.get('/signup', async (req,res) => {
 });
 
 router.get('/login', async (req,res) => {
+
   res.render('login')
 })
 
--- controllers/routes/User.js
+++ controllers/routes/User.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
-router.get('/dashboard', async (req, res) => {
+router.get('/dashboard',  async (req, res) => {
   
   res.render('dashboard', {layout: 'index'});
 });
